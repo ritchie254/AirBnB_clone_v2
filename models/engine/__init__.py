a8b4f0d8d5e7dc7b5e02ae4ffce10f5ae1fb4244
ritchie254 <richardoduor012@gmail.com>
2024-03-20 18:01:06 +0000

    hbnb

---
+++ models/engine/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Instantiates a storage object.
+"""
+from os import getenv
+
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
