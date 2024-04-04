172ae076abb21d5c0b0269169ac5f6e1941afcfa
ritchie254 <richardoduor012@gmail.com>
2024-04-04 03:19:17 -0700

    deploy

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
-
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-# creating directories
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-
-# creating an index.html file in test dir
 sudo touch /data/web_static/releases/test/index.html
-
-# adding some temporary html
 sudo echo "<html>
   <head>
   </head>
@@ -24,14 +18,10 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-#creating a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# giving ownership to /data/ directory to user ubuntu
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve the content of 
-# /data/web_static/current/ to hbnb_static(ex https://mydomainname.tech/hbnb_static)
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
