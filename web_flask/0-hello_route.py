1cca10d158f3c0c8ded1a25b459c83c7ec99b24b
ritchie254 <richardoduor012@gmail.com>
2024-04-19 06:02:20 -0700

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,16 @@
 """
 flask app
 """
-
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
-  """ the root homepage """
-  return("Hello HBNB!")
+    """ the root homepage """
+    return("Hello HBNB!")
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
